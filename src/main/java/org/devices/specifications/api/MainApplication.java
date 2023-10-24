3f6aa6bc8b9c88ccea1978b7353d3b97dbf23a94
Hardik Goel <hardikgoel19@gmail.com>
2023-10-25 00:20:16 +0530

    added email functionality

--- src/main/java/org/devices/specifications/api/MainApplication.java
+++ src/main/java/org/devices/specifications/api/MainApplication.java
@@ -1,9 +1,14 @@
 package org.devices.specifications.api;
 
+import org.devices.specifications.api.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,6 +17,19 @@ public class MainApplication {
 
     public static void main(String[] args){
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Autowired
+    private MailService mailService;
+
+    @Scheduled(cron = "0 0 * ? * *")
+    public void sendEmail() {
+        try {
+            System.out.println("Starting email send");
+            mailService.sendMail();
+        } catch (Exception ignored) {
+
+        }
     }
 
 }
