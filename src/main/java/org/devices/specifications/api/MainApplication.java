a17271c905d34043f328cb41f6bbd9f0646c8198
Hardik Goel <hardikgoel19@gmail.com>
2023-10-25 20:46:52 +0530

    added application controller functionality

--- src/main/java/org/devices/specifications/api/MainApplication.java
+++ src/main/java/org/devices/specifications/api/MainApplication.java
@@ -1,11 +1,15 @@
 package org.devices.specifications.api;
 
 import org.devices.specifications.api.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
@@ -14,6 +18,8 @@ import javax.annotation.PostConstruct;
 @EnableScheduling
 @EnableSwagger2
 public class MainApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
     public static void main(String[] args){
         SpringApplication.run(MainApplication.class, args);
@@ -27,7 +33,19 @@ public class MainApplication {
         try {
             System.out.println("Starting email send");
             mailService.sendMail();
-        } catch (Exception ignored) {
+        } catch (final Exception ignored) {
+
+        }
+    }
+
+    @Scheduled(cron = "* */3 * * * *")
+    public void invokeSelf() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String resourceUrl = "https://api-dev-specs.onrender.com/status";
+            ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
+            logger.info("invokeSelf: {}", response.getBody());
+        } catch (final Exception ignored) {
 
         }
     }
